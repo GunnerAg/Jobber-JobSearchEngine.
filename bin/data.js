20e5bd1b6f2f5a182219f81fdff86937d4cf5d67
Gunner Andersen <gunner.andersen.gil@gmail.com>
2020-08-12 20:39:58 +0200

    j0bber 1.0

--- bin/data.js
+++ bin/data.js
@@ -1,6 +1,6 @@
 module.exports = {
   frontend: ['React','AngularJs','HTML','CSS','Vue.js','JavaScript','TypeScript','Bootstrap','jQuery','SASS'],
-  backend: ['MySQL','Python','C++','Csharp','Java','Node.js','Ruby','RubyOnRails','MariaDB','Symfony','OpenCV','PHP','MongoDB','Django'],
+  backend: ['MySQL','Python','C++','Csharp','Java','Node.js','Ruby','Ruby on rails','MariaDB','Symfony','OpenCV','PHP','MongoDB','Django'],
   devops: ['Ansible','Docker','Bash','Linux Kernel','Git','Debian','AWS','Azure',],
   data: ['Kubernetes','Grafana','Apache Spark','Python','Google Analytics','Hadoop','Kibana','R','TensorFlow']
 }
